8039e829543283b32d592ac7681434e2ff0b0055
Eric W <ericwanjohi.ew@gmail.com>
2020-05-20 12:35:28 +0300

    - Create Constants file with Base_Url and other endpoint strings.

---
+++ app/src/main/java/ke/co/droidsense/virusupdate/constant/Constants.java
@@ -0,0 +1,10 @@
+package ke.co.droidsense.virusupdate.constant;
+
+public class Constants {
+    //Member Variables...
+    public static final String BASE_URL = "https://api.covid19api.com/";
+    public static final String SUMMARY_ENDPOINT = "summary";
+    public static final String COUNTRIES_ENDPOINT = "countries";
+    public static final String DAYONE_COUNTRY_ENDPOINT = ""; // TO be added soon...
+    public static final String STATS_ENDPOINT = "stats";
+}
